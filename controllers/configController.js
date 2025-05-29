6d2f2bf35adaaeb0850d2001cbf8d6750b56f684
moshe <moshe6073163@gmail.com>
2025-05-29 10:54:03 +0300

    fix: add validation for request body in createData function and update data.json with new entries

--- controllers/configController.js
+++ controllers/configController.js
@@ -35,7 +35,7 @@ exports.createData = (req, res) => {
   if (!req.body) {
     return res.status(400).json({ message: 'No data provided or error syntax!' });
   }
-
+  
   const newItem = req.body;
   data.push(newItem);
   writeData(data);
